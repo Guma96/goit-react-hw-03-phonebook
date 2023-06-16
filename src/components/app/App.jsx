50b2421500107abdbd0ca51b5458f748e1be27fa
Guma96 <mateusz.kutarba96@gmail.com>
2023-06-16 10:33:57 +0200

    s

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,7 +5,7 @@ import { ContactList } from '../ContactList';
 import { Filter } from '../Filter';
 import { nanoid } from 'nanoid';
 import defaultContacts from '../../data/contacts.json';
-import { localStorageKey } from '../../patch/localStorageKey.js';
+import { localStorageKey } from '../LocalStorage';
 
 export class App extends Component {
   state = {
