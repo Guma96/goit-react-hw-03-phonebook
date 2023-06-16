0283117cbf8331bb649ee5b6a34fd882859d4933
Guma96 <mateusz.kutarba96@gmail.com>
2023-06-16 10:28:32 +0200

    add localStorageKey.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,11 @@
-import { Component } from "react";
-import { Frame, Container, Title, SubTitle } from "./App.styled";
-import { ContactForm } from "components/ContactForm";
-import { ContactList } from "components/ContactList";
-import { Filter } from "components/Filter";
-import { nanoid } from "nanoid";
+import React, { Component } from 'react';
+import { Frame, Container, Title, SubTitle } from './App.styled';
+import { ContactForm } from '../ContactForm';
+import { ContactList } from '../ContactList';
+import { Filter } from '../Filter';
+import { nanoid } from 'nanoid';
 import defaultContacts from '../../data/contacts.json';
-
+import { localStorageKey } from '../../path/localStorageKey';
 
 export class App extends Component {
   state = {
@@ -13,33 +13,29 @@ export class App extends Component {
     filter: '',
   };
 
-  localStorageKey = 'contacts';
-
-
   componentDidMount() {
-    const savedContacts = localStorage.getItem(this.localStorageKey);
+    const savedContacts = localStorage.getItem(localStorageKey);
 
     if (savedContacts) {
       this.setState({ contacts: JSON.parse(savedContacts) });
     } else {
       this.setState({ contacts: defaultContacts });
     }
-  };
-
-
-  componentDidUpdate(prevState) {
+  }
+  componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
 
     if (prevState.contacts !== contacts) {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(contacts));
+      localStorage.setItem(localStorageKey, JSON.stringify(contacts));
     }
-  }; 
+  }
 
-
-  onSubmitHandler = (newContact) => {
+  onSubmitHandler = newContact => {
     const { contacts } = this.state;
 
-    const isDuplicateName = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase());
+    const isDuplicateName = contacts.some(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
 
     if (isDuplicateName) {
       return alert(`${newContact.name} is already in contacts`);
@@ -49,30 +45,26 @@ export class App extends Component {
 
     const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
     this.setState({
-      contacts: updatedContacts
+      contacts: updatedContacts,
     });
   };
 
-
-  onDeleteHandler = (id) => {
-    this.setState((prev) => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
+  onDeleteHandler = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
 
-
-  onFilterContacts = (event) => {
+  onFilterContacts = event => {
     const filterValue = event.target.value;
     this.setState({ filter: filterValue });
   };
 
-
   render() {
     const { filter } = this.state;
-    const filteredContacts = this.state.contacts.filter((contact) =>
+    const filteredContacts = this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-
 
     return (
       <Container>
@@ -81,14 +73,13 @@ export class App extends Component {
           <ContactForm onSubmit={this.onSubmitHandler} />
 
           <SubTitle>Contacts</SubTitle>
-          <Filter
-            value={filter}
-            onChange={this.onFilterContacts}/>
-          <ContactList contacts={filteredContacts} onDeleteContact={this.onDeleteHandler} />
+          <Filter value={filter} onChange={this.onFilterContacts} />
+          <ContactList
+            contacts={filteredContacts}
+            onDeleteContact={this.onDeleteHandler}
+          />
         </Frame>
       </Container>
     );
   }
-};
-
-
+}
